4447c4608fa78c9a6133c5dfdf787ea36939db1e
Ajeyraj003 <101739665+Ajeyraj003@users.noreply.github.com>
2022-06-22 17:22:53 +0530

    Add files via upload

---
+++ js/Data.js
@@ -0,0 +1,63 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Concepts Solid grey Men's Polo.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Concepts Solid white two shirt's  Men's Polo.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 25,
+    desc: "Concepts Solid white Men's T-shirt Polo.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Black blazer Men's Polo.",
+    img: "images/img-4.jpg",
+  },
+  {
+    id:"seesefsdf",
+    name:"mens t-shirt",
+    price:500,
+    img:"images/product-3.jpg",
+    desc:"t-shirt mens causal",
+  },
+  {
+  
+    id:"sffeete",
+    name:"black mens t-shirt",
+    price:500,
+    img:"images/product-5.jpg",
+    desc:"t-shirt mens causal",
+  },
+  {
+  
+    id:"hgfffeete",
+    name:"grey mens t-shirt",
+    price:500,
+    img:"images/product-4.jpg",
+    desc:"t-shirt mens causal",
+
+  },
+  {
+  
+    id:"hlllllllfffeete",
+    name:"grey mens t-shirt",
+    price:500,
+    img:"images/product-8.jpg",
+    desc:"half sleve  mens causal",
+
+  },
+];
